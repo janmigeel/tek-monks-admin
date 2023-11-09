5267a5f5c9262a3c13a21b04ec373a6639756d7f
Miguel <miguel@nathandigital.com>
2023-11-09 14:14:37 +0800

    Updated code

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,9 @@
 <template>
-  <div v-if="stories.length > 0">
-    <h2 style="margin-left: 10px;">Latest Stories</h2>
-    <ul>
-      <li v-for="(story, index) in stories" :key="index">
-        <a :href="story.link">{{ story.title }}</a>
-      </li>
-    </ul>
-  </div>
-  <div v-else>
-    Loading...
+  <div>
+    <button @click="getLatestStories">Get Latest Stories</button>
+    <div v-for="(story, index) in stories" :key="index" style="padding: 10px;">
+      <a :href="story.link" target="_blank">{{ story.title }}</a>
+    </div>
   </div>
 </template>
 
@@ -19,47 +14,16 @@ export default {
       stories: []
     }
   },
-  mounted() {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'http://localhost:3001/getTimeStories', true)
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          const htmlContent = xhr.responseText
-          const stories = extractStoriesFromHTML(htmlContent)
-          this.stories = stories
-        } else {
-          console.error('Error fetching data')
-        }
+  methods: {
+    async getLatestStories() {
+      try {
+        const response = await fetch('http://localhost:3001/getTimeStories')
+        const data = await response.json()
+        this.stories = data;
+      } catch (error) {
+        console.error('Error fetching latest stories:', error)
       }
     }
-    xhr.send()
   }
-}
-
-function extractStoriesFromHTML(html) {
-  const trimmedHtml = html.trim()
-  if (trimmedHtml.length == 0) {
-    return []
-  }
-  const startIndex = trimmedHtml.indexOf("[")
-  const endIndex = trimmedHtml.lastIndexOf("]")
-  if (startIndex == -1 || endIndex == -1 || endIndex <= startIndex) {
-    return []
-  }
-  const jsonStr = trimmedHtml.substring(startIndex + 1, endIndex)
-  const stories = []
-  const storyStrs = jsonStr.split('},')
-  for (let i = 0; i < storyStrs.length; i++) {
-    const storyStr = storyStrs[i].trim()
-    const titleMatch = storyStr.match(/"title":"(.*?)"/)
-    const linkMatch = storyStr.match(/"link":"(.*?)"/)
-    if (titleMatch && linkMatch) {
-      const title = titleMatch[1]
-      const link = linkMatch[1]
-      stories.push({ title, link })
-    }
-  }
-  return stories
 }
 </script>
